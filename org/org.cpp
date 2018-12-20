6441482d7219815d04aa412c46b23006f456b26a
ThomasHong <jungmin1237@naver.com>
2018-12-20 19:25:07 +0900

    Update org.cpp

--- org/org.cpp
+++ org/org.cpp
@@ -3,7 +3,6 @@
 #include <fstream>
 
 #include <vector>
-#include <list>
 #include <string>
 
 #include <algorithm>
@@ -19,9 +18,74 @@ struct node {
     int level = 1;
     int count = 0;
     shared_ptr<node> parent = nullptr;
-    list<shared_ptr<node>> children;
-    
+
     explicit node(string n) : name(move(n)) {}
+};
+
+using nodeptr = shared_ptr<node>;
+
+class tree{
+    nodeptr p,c;
+    vector<nodeptr> treestr;
+    string sub, boss, result;
+    ostringstream oss;
+    int nodenum = 0;
+public:
+    explicit tree(ifstream& ifs) {
+        ifs >> nodenum;
+        while(--nodenum > 0 && (ifs >> sub >> boss)) {
+            auto bit = find_if(ALL(treestr), [&](const nodeptr &p) { return p->name == boss; });
+            auto sit = find_if(ALL(treestr), [&](const nodeptr &p) { return p->name == sub; });
+            if (bit == treestr.end() && sit == treestr.end()) {
+                p = make_shared<node>(boss);
+                c = make_shared<node>(sub);
+                c->parent = p;
+                treestr.push_back(p);
+                treestr.push_back(c);
+            } else if (bit == treestr.end() && sit != treestr.end()) {
+                p = make_shared<node>(boss);
+                (*sit)->parent = p;
+                treestr.push_back(p);
+            } else if (bit != treestr.end() && sit == treestr.end()) {
+                c = make_shared<node>(sub);
+                c->parent = *bit;
+                treestr.push_back(c);
+            } else if (bit != treestr.end() && sit != treestr.end()) {
+                (*sit)->parent = (*bit);
+            }
+        }
+    }
+
+    void travel() {
+        nodeptr ppl;
+        for(auto &vit:treestr){
+            int adv=0;
+            ppl = vit;
+            while(ppl->parent!=nullptr){
+                ppl = ppl->parent;
+                ppl->count++;
+                adv++;
+            }
+            vit->level = adv;
+        }
+        sort(ALL(treestr), [](const nodeptr& a, const nodeptr& b){
+           if(a->count > b->count) return true;
+           else if(a->count == b->count) {
+               if(a->level < b->level) return true;
+               else if(a->level == b->level) return a->name < b->name;
+               else return false;
+           }
+           else return false;
+        });
+    }
+
+    void treeleaves(ofstream& ofs) {
+        for(auto& it : treestr)
+            oss << it->name << " ";
+        result = oss.str();
+        result.pop_back();
+        ofs << result;
+    }
 };
 
 int main()
@@ -35,26 +99,9 @@ int main()
         return EXIT_FAILURE;
     }
 
-    shared_ptr<node> child;
-    // vector<node> tree;
-    string sub, boss;
-    int nodenum;
-    ifs >> nodenum;
-    while(--nodenum > 0 && (ifs >> sub >> boss)){
-        child = make_shared<node>(sub);
-
-        if(child->parent == nullptr || child->parent->name!=boss)
-            child->parent = make_shared<node>(boss);
-
-        child->level += child->parent->level;
-        child->parent->count += child->count+1;
-        child->parent->children.push_back(child);
-    }
-
-    while(child->parent!=nullptr){
-        cout << child->name << " : level : " << child->level << ", " << child->count << endl;
-        child = child->parent;
-    }
+    tree t(ifs);
+    t.travel();
+    t.treeleaves(ofs);
 
     ofs.close();
     ifs.close();
